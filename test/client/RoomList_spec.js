b7badc6daf6cd58a842c4c244a52f9cf5948d66d
haobing <hobart.cn@gmail.com>
2016-11-14 14:13:32 +0800

    RoomList 组件及其测试

--- test/client/RoomList_spec.js
+++ test/client/RoomList_spec.js
@@ -15,7 +15,7 @@ import TestUtil,{
 } from "react-addons-test-utils"
 
 describe('RoomList组件',()=>{
-  it('render roomlist',()=>{
+  it('渲染roomlist',()=>{
     const rooms = fromJS([
       {id:'0',name: 'room', owner: 'haobing'},
       {id:'1',name: 'room2', owner: 'terry'},
@@ -29,5 +29,28 @@ describe('RoomList组件',()=>{
     expect($rooms.length).to.equal(2)
     const $active = scryRenderedDOMComponentsWithClass(component,'active')
     expect($active.length).to.equal(1)
+  })
+
+  it('能够切换房间',()=>{
+    const rooms = fromJS([
+      {id:'0',name:'room1',owner:'haobing'},
+      {id:'1',name:'room2',owner:'terry'},
+    ])
+    var currentRoom = '0'
+    function switchRoom(id) {
+      // console.log('change id:',id)
+      currentRoom = id
+    }
+    const RoomListElement = (
+      <RoomList rooms={rooms}
+        currentRoom={currentRoom}
+        switchRoom={switchRoom}
+      />
+    )
+
+    const component = renderIntoDocument(RoomListElement)
+    const $rooms = scryRenderedDOMComponentsWithTag(component,'a')
+    Simulate.click(ReactDOM.findDOMNode($rooms[1]))
+    expect(currentRoom).to.equal('1')
   })
 })
